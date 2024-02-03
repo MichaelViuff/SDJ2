53a11b7a4a1b3b66852a3777f3278c5ee8174c5f
MichaelViuff <128811537+MichaelViuff@users.noreply.github.com>
2024-02-03 10:58:34 +0100

    Update Letters.java

--- 01 Threads 1/Examples/Letters.java
+++ 01 Threads 1/Examples/Letters.java
@@ -1,14 +1,14 @@
 public class Letters implements Runnable
 {
 
-  @Override public void run()
-  {
-    while(true)
-      {
-        for (int i = 0; i < 10; i++)
+    @Override public void run()
+    {
+        while(true)
         {
-          System.out.println((char)i+97);
+            for (int i = 0; i < 10; i++)
+            {
+                System.out.println((char)(i+97));
+            }
         }
-      }
-  }
+    }
 }
