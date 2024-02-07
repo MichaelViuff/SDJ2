b35b57fd94a129de8647ffc276046f1efa299bd3
MichaelViuff <128811537+MichaelViuff@users.noreply.github.com>
2024-02-07 13:29:06 +0100

    Update Main.java

--- 03 Observer Pattern/Examples/Main.java
+++ 03 Observer Pattern/Examples/Main.java
@@ -7,8 +7,8 @@ public class Test
     Car car1 = new Car(1, trafficLight);
     Car car2 = new Car(2, trafficLight);
     
-    trafficLight.addPropertyChangeListener(car1);
-    trafficLight.addPropertyChangeListener(car2);
+    trafficLight.addPropertyChangeListener(car1, "Lightchanged");
+    trafficLight.addPropertyChangeListener(car2, "Lightchanged");
 
     trafficLight.setColor("GREEN");
     trafficLight.setColor("YELLOW");
