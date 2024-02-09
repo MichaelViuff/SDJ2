825c14549f3982b18a027c7f9221505c30a795a4
MichaelViuff <128811537+MichaelViuff@users.noreply.github.com>
2024-02-09 09:51:44 +0100

    Update Main.java

--- 03 Observer Pattern/Examples/Main.java
+++ 03 Observer Pattern/Examples/Main.java
@@ -4,12 +4,8 @@ public class Main
     {
         TrafficLight trafficLight = new TrafficLight();
         Car car1 = new Car(1);
-        Car car2 = new Car(2);
-        Car car3 = new Car(3);
 
         trafficLight.addCar(car1);
-        trafficLight.addCar(car2);
-        trafficLight.addCar(car3);
 
         try
         {
