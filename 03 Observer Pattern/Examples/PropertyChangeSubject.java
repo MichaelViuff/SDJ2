6c23ead608fab2baeb2b9501847f10f4bae83d9b
MichaelViuff <128811537+MichaelViuff@users.noreply.github.com>
2024-02-09 08:42:58 +0100

    Add files via upload

---
+++ 03 Observer Pattern/Examples/PropertyChangeSubject.java
@@ -0,0 +1,11 @@
+import java.beans.PropertyChangeListener;
+
+public interface PropertyChangeSubject
+{
+
+  public void addPropertyChangeListener(PropertyChangeListener listener);
+  public void addPropertyChangeListener(String name, PropertyChangeListener listener);
+  public void removePropertyChangeListener(PropertyChangeListener listener);
+  public void removePropertyChangeListener(String name, PropertyChangeListener listener);
+
+}
