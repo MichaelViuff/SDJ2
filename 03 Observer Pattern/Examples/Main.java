caaf53455ed8fac3c14f87fa5459d4bc2ccea6c1
MichaelViuff <128811537+MichaelViuff@users.noreply.github.com>
2024-02-09 08:55:57 +0100

    Update Main.java

--- 03 Observer Pattern/Examples/Main.java
+++ 03 Observer Pattern/Examples/Main.java
@@ -1,19 +1,23 @@
-public class Test
+public class Main
 {
-  public static void main(String[] args)
-  {
-    TrafficLight trafficLight = new TrafficLight();
-    
-    Car car1 = new Car(1, trafficLight);
-    Car car2 = new Car(2, trafficLight);
-    
-    trafficLight.addPropertyChangeListener("Lightchanged", car1);
-    trafficLight.addPropertyChangeListener("Lightchanged", car2);
+    public static void main(String[] args)
+    {
+        TrafficLight trafficLight = new TrafficLight();
+        Car car1 = new Car(1);
+        Car car2 = new Car(2);
+        Car car3 = new Car(3);
 
-    trafficLight.setColor("GREEN");
-    trafficLight.setColor("YELLOW");
-    trafficLight.setColor("RED");
-    trafficLight.setColor("YELLOW");
-    trafficLight.setColor("GREEN");
-  }
+        trafficLight.addCar(car1);
+        trafficLight.addCar(car2);
+        trafficLight.addCar(car3);
+
+        try
+        {
+            trafficLight.start();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
