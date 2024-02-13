e2d317a0fe26df4199f2f57e622d388f2b0d3782
MichaelViuff <128811537+MichaelViuff@users.noreply.github.com>
2024-02-13 12:01:47 +0100

    Update Main.java

--- 03 Observer Pattern/Examples/JavaFX Charts/Main.java
+++ 03 Observer Pattern/Examples/JavaFX Charts/Main.java
@@ -2,9 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URL;
 
 public class Main extends Application
 {
