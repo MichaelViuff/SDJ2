85e9736e87635d4fdc2c15b9b6833be6de3c628e
MichaelViuff <128811537+MichaelViuff@users.noreply.github.com>
2024-02-14 14:45:03 +0100

    Update BarChartViewController.java

--- 03 Observer Pattern/Examples/JavaFX Charts/BarChartViewController.java
+++ 03 Observer Pattern/Examples/JavaFX Charts/BarChartViewController.java
@@ -1,5 +1,6 @@
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
@@ -7,47 +8,57 @@ import javafx.scene.chart.XYChart;
 
 import java.beans.PropertyChangeEvent;
 
-public class BarChartViewController
-{
-  @FXML
-  private BarChart<String, Integer> barChart;
-  @FXML
-  private CategoryAxis xAxis = new CategoryAxis();
-  @FXML
-  private NumberAxis yAxis = new NumberAxis();
+public class BarChartViewController {
 
-  private XYChart.Series<String, Integer> dataSeries;
+    @FXML
+    private BarChart<String, Integer> barChart;
+    @FXML
+    private CategoryAxis xAxis = new CategoryAxis();
+    @FXML
+    private NumberAxis yAxis = new NumberAxis();
 
-  public BarChartViewController(DataModel model)
-  {
-    model.addPropertyChangeListener("DataChange", this::updateData);
-  }
+    private XYChart.Series<String, Integer> dataSeries;
 
-  public void initialize()
-  {
-    barChart.setTitle("Data Representation");
-    xAxis.setLabel("Colour");
-    yAxis.setLabel("Value");
-    yAxis.setAutoRanging(false);
-    yAxis.setLowerBound(0);
-    yAxis.setUpperBound(100);
-
-    dataSeries = new XYChart.Series();
-    dataSeries.setName("Colour Values");
-    dataSeries.getData().add(new XYChart.Data("Red", 0));
-    dataSeries.getData().add(new XYChart.Data("Green", 0));
-    dataSeries.getData().add(new XYChart.Data("Yellow", 0));
-    barChart.getData().addAll(dataSeries);
-  }
-
-  private void updateData(PropertyChangeEvent evt)
-  {
-    Platform.runLater(() ->
+    public BarChartViewController(DataModel model)
     {
-      int[] data = (int[]) evt.getNewValue();
-      dataSeries.getData().get(0).setYValue(data[0]);
-      dataSeries.getData().get(1).setYValue(data[1]);
-      dataSeries.getData().get(2).setYValue(data[2]);
-    });
-  }
+        model.addPropertyChangeListener("DataChange", this::onColourValueChange);
+    }
+
+    private void onColourValueChange(PropertyChangeEvent event)
+    {
+        Platform.runLater(() ->
+        {
+            int[] newValues = (int[]) event.getNewValue();
+            int newRed = newValues[0];
+            int newGreen = newValues[1];
+            int newBlue = newValues[2];
+            dataSeries.getData().get(0).setYValue(newRed);
+            dataSeries.getData().get(1).setYValue(newGreen);
+            dataSeries.getData().get(2).setYValue(newBlue);
+        });
+    }
+
+    public void initialize()
+    {
+        barChart.setTitle("Data Representation");
+        barChart.setLegendVisible(false);
+        xAxis.setLabel("Colours");
+        yAxis.setLabel("Value");
+        yAxis.setAutoRanging(false);
+        yAxis.setLowerBound(0);
+        yAxis.setUpperBound(100);
+
+        dataSeries = new XYChart.Series();
+        dataSeries.getData().add(new XYChart.Data("Red", 0));
+        dataSeries.getData().add(new XYChart.Data("Green", 0));
+        dataSeries.getData().add(new XYChart.Data("Blue", 0));
+        barChart.getData().add(dataSeries);
+
+        Node node = barChart.lookup(".data0.chart-bar");
+        node.setStyle("-fx-bar-fill: red");
+        node = barChart.lookup(".data1.chart-bar");
+        node.setStyle("-fx-bar-fill: green");
+        node = barChart.lookup(".data2.chart-bar");
+        node.setStyle("-fx-bar-fill: blue");
+    }
 }
