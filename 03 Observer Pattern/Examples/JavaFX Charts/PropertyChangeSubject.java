fb5306f7b9bd0d0b792ce66d517fd8794af4b9c7
MichaelViuff <128811537+MichaelViuff@users.noreply.github.com>
2024-02-14 14:46:39 +0100

    Update PropertyChangeSubject.java

--- 03 Observer Pattern/Examples/JavaFX Charts/PropertyChangeSubject.java
+++ 03 Observer Pattern/Examples/JavaFX Charts/PropertyChangeSubject.java
@@ -2,10 +2,8 @@ import java.beans.PropertyChangeListener;
 
 public interface PropertyChangeSubject
 {
-
-  public void addPropertyChangeListener(PropertyChangeListener listener);
-  public void addPropertyChangeListener(String name, PropertyChangeListener listener);
-  public void removePropertyChangeListener(PropertyChangeListener listener);
-  public void removePropertyChangeListener(String name, PropertyChangeListener listener);
-
+  void addPropertyChangeListener(PropertyChangeListener listener);
+  void addPropertyChangeListener(String name, PropertyChangeListener listener);
+  void removePropertyChangeListener(PropertyChangeListener listener);
+  void removePropertyChangeListener(String name, PropertyChangeListener listener);
 }
